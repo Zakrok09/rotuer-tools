4849b8e9805e7e76b723abb664e446dc898a9723
Zakrok09 <eccyboo@protonmail.com>
2022-09-02 22:26:45 +0200

    Initial release

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
-	preprocess: preprocess()
+	preprocess: preprocess(),
+
+	kit: {  // some settings for adapter-static:
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+	}
 };
 
 export default config;
